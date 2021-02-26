a2cd1731c13d4ea8b9c25e250b28532c24039e42
Michael Cesena <cesen1me@cmich.edu>
2021-02-25 20:12:53 -0500

    New changes

---
+++ scraper.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import dateparser as dp
+
+# Variables
+title = ''
+genres_html = []
+genres = []
+release =''
+
+# Gets info for movie from IMDB
+URL = 'https://www.imdb.com/title/tt1130884/?ref_=fn_al_tt_1'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, features="html.parser")
+
+# Gets title of movie
+title = soup.find('h1', class_='')
+
+# Gets list of genres
+genres_html = (soup.find('div', class_='subtext').find_all('a'))
+
+# Adds html genres to the genre list
+for i in genres_html[:-1]:
+    genres.append(i.text)
+
+# Gets and formats the release date
+release = genres_html[-1].text
+release = release.split('(')[0]
+release = dp.parse(release)
+
